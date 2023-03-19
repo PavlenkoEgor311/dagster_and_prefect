a775c7ad5f05f8774debd168f0fe7408128b8e19
PavlenkoEgor311 <pavlenkoegor2@gmail.com>
2023-03-19 18:34:22 +0400

    refactor

--- main.py
+++ main.py
@@ -2,35 +2,35 @@ from dagster import asset, materialize, op
 import pandas as pd
 from urllib.parse import urlparse
 
-path_in, path_out = "original.csv", "norm.csv"
+pathIn, pathOut = "original.csv", "norm.csv"
 
 
-def fix_path(f_in, f_out):
-    global path_in, path_out
-    path_in = f_in
-    path_out = f_out
+def fixPath(inF, outF):
+    global pathIn, pathOut
+    pathIn = inF
+    pathOut = outF
 
 
 @asset
-def load_data():
-    df = pd.read_csv(path_in)
+def loadData():
+    df = pd.read_csv(pathIn)
     return df
 
 
 @asset
-def get_domain(load_data):
-    load_data['domain'] = [urlparse(i).netloc for i in load_data['url']]
-    return load_data
+def getDomain(loadData):
+    loadData['domain'] = [urlparse(i).netloc for i in loadData['url']]
+    return loadData
 
 
 @asset
-def upload_data(get_domain):
-    get_domain.to_csv(path_out)
+def uploadData(getDomain):
+    getDomain.to_csv(pathOut)
 
 
-def run_assets():
-    materialize(assets=[load_data, get_domain, upload_data])
+def run():
+    materialize(assets=[loadData, getDomain, uploadData])
 
 
 if __name__ == '__main__':
-    run_assets()
+    run()
--- my_prefect.py
+++ my_prefect.py
@@ -2,32 +2,32 @@ from prefect import task, flow
 import pandas as pd
 from urllib.parse import urlparse
 
-path_in, path_out = "original.csv", "norm.csv"
+pathIn, pathOut = "original.csv", "norm.csv"
 
 
 @task
-def load_data(path: str) -> pd.DataFrame:
+def loadData(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
 
 
 @task
-def get_domain(df: pd.DataFrame) -> pd.DataFrame:
+def getDomain(df: pd.DataFrame) -> pd.DataFrame:
     df['domain'] = [urlparse(i).netloc for i in df['url']]
     return df
 
 
 @task
-def upload_data(df: pd.DataFrame, path: str):
+def uploadData(df: pd.DataFrame, path: str):
     df.to_csv(path)
 
 
 @flow(name="task_prefect")
-def my_flow():
-    data = load_data(path=path_in)
-    data_domains = get_domain(df=data)
-    upload_data(df=data_domains, path=path_out)
+def run():
+    data = loadData(path=pathIn)
+    dataDomains = getDomain(df=data)
+    uploadData(df=dataDomains, path=pathOut)
 
 
 if __name__ == '__main__':
-    my_flow()
+    run()
